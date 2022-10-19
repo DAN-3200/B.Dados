a5af3491254b15c5d1b4074158d26fb3777cb16b
Thatoon <Tunes.750@outlook.com>
2022-10-19 11:19:39 -0300

    Nuevo3

--- src/main/java/com/banco/banco/controller/UsuarioController.java
+++ src/main/java/com/banco/banco/controller/UsuarioController.java
@@ -1,7 +1,19 @@
 package com.banco.banco.controller;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.web.bind.annotation.RestController;
     import com.banco.banco.banco.UsuarioBanco;
+    import com.banco.banco.entidade.Usuario;
+    import java.util.List;
+
 @RestController
 public class UsuarioController{
+    @Autowired
+    private UsuarioBanco acessoBanco;
+
+    @GetMapping("/usuarios")
+    public List<Usuario> ListarUsuarios(){
+        return (List<Usuario>) acessoBanco.findAll();
+    }
     
 }
