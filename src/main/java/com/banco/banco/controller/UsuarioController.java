02ddeb1dd7cb4eed49c7596f75e3fcd8d1254b86
Thatoon <Tunes.750@outlook.com>
2022-10-20 09:24:28 -0300

    nuevo

--- src/main/java/com/banco/banco/controller/UsuarioController.java
+++ src/main/java/com/banco/banco/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.banco.banco.controller;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
     import org.springframework.web.bind.annotation.RestController;
     import com.banco.banco.banco.UsuarioBanco;
     import com.banco.banco.entidade.Usuario;
@@ -22,5 +24,9 @@ public class UsuarioController{
     public Optional<Usuario> peguePorId(@PathVariable int id){
         return acessoBanco.findById(id);
     }
-    
+
+    @PostMapping("/cadastrar")
+    public void cadastrar(@RequestBody Usuario NovoUsuario){
+        acessoBanco.save(NovoUsuario);
+    }
 }
