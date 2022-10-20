0b908e37247ccee8d795d8284b5d50e23b6c2e52
Thatoon <Tunes.750@outlook.com>
2022-10-20 09:04:52 -0300

    Nuevo

--- src/main/java/com/banco/banco/controller/UsuarioController.java
+++ src/main/java/com/banco/banco/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package com.banco.banco.controller;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
     import org.springframework.web.bind.annotation.RestController;
     import com.banco.banco.banco.UsuarioBanco;
     import com.banco.banco.entidade.Usuario;
     import java.util.List;
+    import java.util.Optional;
 
 @RestController
 public class UsuarioController{
@@ -14,6 +16,11 @@ public class UsuarioController{
     @GetMapping("/usuarios")
     public List<Usuario> ListarUsuarios(){
         return (List<Usuario>) acessoBanco.findAll();
+    }
+
+    @GetMapping("/ususario/{id}")
+    public Optional<Usuario> peguePorId(@PathVariable int id){
+        return acessoBanco.findById(id);
     }
     
 }
